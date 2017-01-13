18969bdbfb6c5264c7bd082d4faeb763921cb399
Erlon <erlon.freitas@gmail.com>
2017-01-13 10:15:45 -0200

    Meu primeiro commit

--- src/PortalAxit/Controllers/HomeController.cs
+++ src/PortalAxit/Controllers/HomeController.cs
@@ -6,30 +6,39 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace PortalAxit.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
-        }
+      return View();
     }
+
+    public IActionResult About()
+    {
+      ViewData["Message"] = "Your application description page.";
+
+      return View();
+    }
+
+    public IActionResult xptosd()
+    {
+      return null;
+    }
+
+    public IActionResult xpto()
+    {
+      return null;
+    }
+    public IActionResult Contact()
+    {
+      ViewData["Message"] = "Your contact page.";
+
+      return View();
+    }
+
+    public IActionResult Error()
+    {
+      return View();
+    }
+  }
 }
